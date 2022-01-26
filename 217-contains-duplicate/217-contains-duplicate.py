f3989818fc5e9298f5c37ed8d82a2ced715db30f
ganavee <itsganavijayaram@gmail.com>
2022-01-26 16:10:56 +0530

    Added Brute Force code

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,7 +1,18 @@
+'''Using Array'''
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         nums.sort()
         for i in range(len(nums)-1):
             if(nums[i] == nums[i+1]):
                 return True
+        return False
+
+''' Brute Force '''
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        length = len(nums)
+        for i in range(length-1):
+            for j in  range(i+1, length):
+                if(nums[i] == nums[j]):
+                    return True
         return False
