6eeb89a66655e99d902fdb64a02e0b61e029989c
Ganavi J <ganavi.1rn15cs041@gmail.com>
2022-02-24 11:32:36 +0530

    Time: 44 ms (59.18%), Space: 14 MB (81.06%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -1,24 +1,5 @@
 class Solution:
-    def converttodec(self, binary):
-        j = 0
-        dec = 0 
-        for i in range(len(binary)-1, -1, -1):
-            dec += int(binary[i])*(2**j)
-            j += 1
-        return dec
-    def converttobin(self, decimal):
-        lst = []
-        if(decimal == 0):
-            return "0"
-        while(decimal):
-            rem = decimal % 2
-            lst.insert(0, rem)
-            decimal //= 2
-        str1 = ''.join(str(e) for e in lst)
-        return(str1)
     def addBinary(self, a: str, b: str) -> str:
-        #dec = (self.converttodec(a) +  self.converttodec(b))
-        #return(self.converttobin(dec))
         lista = list(a)
         listb = list(b)
         if(len(a) < len(b)):
