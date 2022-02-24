c62468c26aaf5fdd1ea8e6c1229ae95953f98cd7
Ganavi J <ganavi.1rn15cs041@gmail.com>
2022-02-24 11:31:53 +0530

    Time: 28 ms (96.91%), Space: 13.9 MB (81.06%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -17,6 +17,31 @@ class Solution:
         str1 = ''.join(str(e) for e in lst)
         return(str1)
     def addBinary(self, a: str, b: str) -> str:
-        dec = (self.converttodec(a) +  self.converttodec(b))
-        return(self.converttobin(dec))
+        #dec = (self.converttodec(a) +  self.converttodec(b))
+        #return(self.converttobin(dec))
+        lista = list(a)
+        listb = list(b)
+        if(len(a) < len(b)):
+            length = len(b)-len(a)
+            for i in range(length):
+               lista.insert(0, 0)
+        else:
+            length = len(a)-len(b)
+            for i in range(length):
+               listb.insert(0, 0)
+        carry = 0
+        listc = []
+        for i in range(len(lista)-1, -1, -1):
+            xor = int(lista[i]) ^ int(listb[i])
+            final = xor ^ carry
+            if((xor == 0 and int(lista[i]) == 1) or (final == 0 and carry ==  1)):
+                carry = 1
+            else:
+                carry = 0
+            listc.insert(0, final)
+        if(carry == 1):
+            listc.insert(0, carry)
+        str1 = ''.join(str(e) for e in listc)
+        return str1
+
         
