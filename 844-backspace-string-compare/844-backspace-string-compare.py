833d3054263463865cee2b1a03eddeb09338cbb1
Ganavi J <ganavi.1rn15cs041@gmail.com>
2022-03-02 15:22:44 +0530

    Time: 55 ms (23.55%), Space: 13.9 MB (61.22%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.py
+++ 844-backspace-string-compare/844-backspace-string-compare.py
@@ -1,23 +1,51 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        stack1 = []
-        length = 0
-        for i in s:
-            if(i == "#"):
-                if(length != 0):
-                    stack1.pop()
-                    length -= 1
+        start1 = len(s)-1
+        start2 = len(t)-1
+        skip1 = skip2 = 0
+        sen1 = sen2 = 0
+        while(start1 != -1 or start2 != -1): #check
+            if( (start1 != -1 and start2 != -1)and (s[start1] == "#" and t[start2] == "#")):
+                skip1 += 1
+                skip2 += 1
+                start1 -= 1
+                start2 -= 1
+                continue
+            if(start1 != -1):
+                if(s[start1] == "#"):
+                    skip1 += 1
+                    start1 -= 1
+                elif(start1 != -1 and skip1 != 0):
+                    start1 -= 1
+                    skip1 -= 1
+                elif(start2 != -1):
+                    sen1 = 1
+                else:
+                    return False
+                    start1 = -1
+            if(start2 != -1):
+                if(t[start2] == "#"):
+                    skip2 += 1
+                    start2 -= 1
+                elif(start2 != -1 and skip2 != 0):
+                    start2 -= 1
+                    skip2 -= 1
+                elif(start1 != -1):
+                    sen2 = 1
+                else:
+                    return False
+                    start2 = -1
+            if(sen1 == 1 and sen2 == 1):
+                if(s[start1] == t[start2]):
+                    start1 -= 1
+                    start2 -= 1
+                    sen1 = 0
+                    sen2 = 0
+                else:
+                    return False
             else:
-                stack1.append(i)
-                length += 1
-        stack2 = []
-        length = 0
-        for i in t:
-            if(i == "#"):
-                if(length != 0):
-                    stack2.pop()
-                    length -= 1
-            else:
-                stack2.append(i)
-                length += 1
-        return(stack1 == stack2)
+                sen1 = 0
+                sen2 = 0
+        if((start1) == (start2)):
+            return True
+        return False
