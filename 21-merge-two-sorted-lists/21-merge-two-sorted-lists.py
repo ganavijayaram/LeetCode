1863212ca57d8491c2b4e6596e967836f53be8ff
Ganavi Jayaram <ganavi.1rn15cs041@gmail.com>
2022-07-12 15:54:37 +0530

    Time: 64 ms (5.35%), Space: 13.3 MB (81.95%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -1,45 +1,51 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if(list1 == None and list2 == None):
-            return None
+class Solution(object):
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        
+    def insert(self, val):
+        if(self.head == None):
+            self.head = ListNode(val)
+            self.tail = self.head
+            #self.display(self.head)
+            return self.head
+        self.tail.next = ListNode(val)
+        self.tail = self.tail.next
+        #self.display(self.head)
+        return self.head
+        
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
         if(list1 == None):
             return list2
         if(list2 == None):
             return list1
-        if(list1.val >= list2.val):
-            start1 = list1
-            start2 = list2
-            y = 1
-        else:
-            start1 = list2
-            start2 = list1
-            y = 2
-        prev2 = start2
-        x = 0
-        next1 = None
-        while((start1) and (start2)):
-            while((start1) and (start2) and (start1.val >= start2.val)):
-                prev2 = start2
-                start2 = start2.next
-                x = 1
-            if(x == 1):
-                x = 0
-                continue
-            next1 = start1.next
-            prev2.next = start1
-            prev2 = start1
-            prev2.next = start2
-            start1 = next1
-        while(start1):
-            next1 = start1.next
-            prev2.next = start1
-            prev2 = start1
-            start1 = next1
-        if(y == 1):
-            return list2
-        return list1
+        if(list1 == None and list2 == None):
+            return list1
+        while(list1 != None and list2 != None):
+            if(list1.val < list2.val):
+                self.insert(list1.val)
+                list1 = list1.next
+            else:
+                self.insert(list2.val)
+                list2 = list2.next
+            #self.display(self.head)
+        while(list1 != None):
+            self.insert(list1.val)
+            list1 = list1.next
+        while(list2 != None):
+            self.insert(list2.val)
+            list2 = list2.next
+        #self.display(self.head)
+        return self.head
+    
+
