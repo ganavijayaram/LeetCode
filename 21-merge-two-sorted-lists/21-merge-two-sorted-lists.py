8174e112d527edc037a4dde5de74c54259b31ce7
Ganavi Jayaram <ganavi.1rn15cs041@gmail.com>
2022-07-13 05:47:33 +0530

    Time: 25 ms (86.81%), Space: 13.6 MB (31.73%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -4,48 +4,25 @@
 #         self.val = val
 #         self.next = next
 class Solution(object):
-    def __init__(self):
-        self.head = None
-        self.tail = None
-        
-    def insert(self, val):
-        if(self.head == None):
-            self.head = ListNode(val)
-            self.tail = self.head
-            #self.display(self.head)
-            return self.head
-        self.tail.next = ListNode(val)
-        self.tail = self.tail.next
-        #self.display(self.head)
-        return self.head
-        
     def mergeTwoLists(self, list1, list2):
         """
         :type list1: Optional[ListNode]
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        if(list1 == None):
-            return list2
-        if(list2 == None):
-            return list1
-        if(list1 == None and list2 == None):
-            return list1
-        while(list1 != None and list2 != None):
-            if(list1.val < list2.val):
-                self.insert(list1.val)
+        dummy = ListNode(0)
+        result = dummy
+        while (list1 != None and list2 != None):
+            if(list1.val <= list2.val):
+                dummy.next = list1
                 list1 = list1.next
             else:
-                self.insert(list2.val)
+                dummy.next = list2
                 list2 = list2.next
-            #self.display(self.head)
-        while(list1 != None):
-            self.insert(list1.val)
-            list1 = list1.next
-        while(list2 != None):
-            self.insert(list2.val)
-            list2 = list2.next
-        #self.display(self.head)
-        return self.head
-    
-
+            dummy = dummy.next
+        if(list1 == None):
+            dummy.next = list2
+        else:
+            dummy.next = list1
+        return result.next
+        
