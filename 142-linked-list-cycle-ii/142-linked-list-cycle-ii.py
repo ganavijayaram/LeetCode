b7b34faa2000e3b5a1249d638d63a1a2664fd24b
Ganavi Jayaram <ganavi.1rn15cs041@gmail.com>
2022-07-14 06:55:28 +0530

    Time: 39 ms (91.67%), Space: 19.8 MB (40.39%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
@@ -10,13 +10,30 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        temp = head
-        counter = 0
-        D = {}
-        while(temp):
-            if(temp in D):
-                return temp
-            D[temp] = counter
-            counter += 1
-            temp = temp.next
+        slow = fast = head
+        while(fast and fast.next):
+            slow = slow.next
+            fast = fast.next.next
+            if(slow == fast):
+                cycle_len = self.length(slow, fast)
+                return self.start(head, cycle_len)
+                break
         return None
+    
+    def start(self, head, length):
+        first = second = head
+        for i in range(length):
+            second = second.next
+        while(first != second):
+            first = first.next
+            second = second.next
+        return first
+        pass
+    
+    def length(self, slow, fast):
+        slow = slow.next
+        count = 1
+        while(slow != fast):
+            slow = slow.next
+            count += 1
+        return count
